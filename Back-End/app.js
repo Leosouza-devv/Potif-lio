09df90f40d3abed23e86a46ba68831377d194365
Leosouza-devv <106339564+leosouza-devv@users.noreply.github.com>
2026-04-01 14:18:57 -0300

    updade with node js + organization of folders

---
+++ Back-End/app.js
@@ -0,0 +1,24 @@
+// app.js
+const express = require('express');
+const connection = require('./db');  // Importar a conexão com o banco de dados
+
+// Criar a aplicação Express
+const app = express();
+
+// permite que arquivos estaticos funcionem ao mesmo tempo que o node.js
+app.use(express.static(__dirname + '/src'));
+
+//(INICIO) Rotas para as paginas HTML
+
+// Rota para pagina Inicio/index, responsavel por iniciar o site na pagina inicial
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/src/pages/index.html'); // Vai servir o arquivo HTML
+});
+
+//(FIM) Rotas para as paginas HTML
+
+
+// servidor na porta 3000
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta 3000: http://localhost:3000");
+});
---
+++ Front-End/scripts/commos/Loading.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let quadrado_principal = document.getElementById('quadrado_principal');
+
+let quadrado_1 = document.getElementById('quadrado_1'); 
+let quadrado_2 = document.getElementById('quadrado_2'); 
+let quadrado_3 = document.getElementById('quadrado_3'); 
+
+
+
+quadrado_principal = [
+    [quadrado_1, quadrado_2],
+    [quadrado_3, 0]
+];
+
