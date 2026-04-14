4854711079b9b6212a7ca45b7c2773d3c005458c
Leosouza-devv <106339564+leosouza-devv@users.noreply.github.com>
2026-04-14 16:50:01 -0300

    update sass

--- back_end/app.js
+++ back_end/app.js
@@ -1,18 +1,18 @@
 // app.js
 const express = require('express');
-const connection = require('./back_end/database/db');  // Importar a conexão com o banco de dados
-
+const connection = require('./database/db');  // Importar a conexão com o banco de dados
+const path = require('path');
 // Criar a aplicação Express
 const app = express();
 
 // permite que arquivos estaticos funcionem ao mesmo tempo que o node.js
-app.use(express.static(__dirname + '/front_end'));
+app.use(express.static(path.join(__dirname, '../front_end')));
 
 //(INICIO) Rotas para as paginas HTML
 
 // Rota para pagina Inicio/index, responsavel por iniciar o site na pagina inicial
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/front_end/pages/index.html'); // Vai servir o arquivo HTML
+  res.sendFile(path.join(__dirname, '../front_end/index.html')); // Vai servir o arquivo HTML
 });
 
 
