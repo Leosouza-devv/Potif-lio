cc90d801c362b5f414bf1de373cbb280bceddecb
Leosouza-devv <106339564+leosouza-devv@users.noreply.github.com>
2026-04-14 15:57:07 -0300

    update

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // app.js
 const express = require('express');
-const connection = require('./Back-End/database/db');  // Importar a conexão com o banco de dados
+const connection = require('./back_end/database/db');  // Importar a conexão com o banco de dados
 
 // Criar a aplicação Express
 const app = express();
 
 // permite que arquivos estaticos funcionem ao mesmo tempo que o node.js
-app.use(express.static(__dirname + '/Front-End'));
+app.use(express.static(__dirname + '/front_end'));
 
 //(INICIO) Rotas para as paginas HTML
 
 // Rota para pagina Inicio/index, responsavel por iniciar o site na pagina inicial
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/Front-End/pages/index.html'); // Vai servir o arquivo HTML
+  res.sendFile(__dirname + '/front_end/pages/index.html'); // Vai servir o arquivo HTML
 });
 
 
---
+++ front_end/shared/scripts/commos/Config_padrão.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// Ano do Copy na pagina
+const ano = new Date().getFullYear();
+document.getElementById("ano_copy").textContent = ano;
+
